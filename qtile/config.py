bb9c903148829f9e1ffe474d6efa461806c747b0
Ronaldo <oldanorsh@gmail.com>
2024-01-03 12:48:25 +0000

    feat(config): cleaning keys

--- qtile/config.py
+++ qtile/config.py
@@ -1,29 +1,3 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from typing import List  # noqa: F401
 import os
 import subprocess
@@ -31,14 +5,12 @@ import subprocess
 from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-
-
-from color import colors
+from keys import keys, mod
 from font import font
 from layouts import custom_layouts, floating
 from bar import init_screens
 
-mod = "mod4"
+# terminal = guess_terminal()
 terminal = "kitty"
 
 float_types = ["dialog"]
@@ -92,115 +64,6 @@ def dialogs(window):
     ):
         window.floating = True
 
-
-keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key(
-        [mod, "mod1"],
-        "space",
-        lazy.layout.next(),
-        desc="Move window focus to other window",
-    ),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key(
-        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
-    ),
-    Key(
-        [mod, "shift"],
-        "l",
-        lazy.layout.shuffle_right(),
-        desc="Move window to the right",
-    ),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key(
-        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
-    ),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key([mod], "t", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([], "XF86Calculator", lazy.spawn("gnome-calculator"), desc="Open calculator"),
-    # scripts
-    Key(
-        [mod],
-        "space",
-        lazy.spawn("sh " + os.path.expanduser("~/.scripts/rofi-launchpad.sh")),
-        desc="Rofi",
-    ),
-    Key(
-        [mod],
-        "p",
-        lazy.spawn("sh " + os.path.expanduser("~/.scripts/rofi-powermenu.sh")),
-        desc="Powermenu",
-    ),
-    Key(
-        [],
-        "XF86AudioMute",
-        lazy.spawn("sh " + os.path.expanduser("~/.scripts/sound/volume-mute.sh")),
-        desc="Mute volume",
-    ),
-    Key(
-        [],
-        "XF86AudioLowerVolume",
-        lazy.spawn("sh " + os.path.expanduser("~/.scripts/sound/volume-down.sh")),
-        desc="Lower volume",
-    ),
-    Key(
-        [],
-        "XF86AudioRaiseVolume",
-        lazy.spawn("sh " + os.path.expanduser("~/.scripts/sound/volume-up.sh")),
-        desc="Raise volume",
-    ),
-    Key(
-        [],
-        "XF86MonBrightnessUp",
-        lazy.spawn("sh " + os.path.expanduser("~/.scripts/backlight/increase.sh")),
-        desc="Increase brightness",
-    ),
-    Key(
-        [],
-        "XF86MonBrightnessDown",
-        lazy.spawn("sh " + os.path.expanduser("~/.scripts/backlight/decrease.sh")),
-        desc="Decrease brightness",
-    ),
-    Key(
-        [],
-        "Print",
-        lazy.spawn("xfce4-screenshooter"),
-        desc="Take a screenshot",
-    ),
-    Key([mod], "c", lazy.spawn("clipcat-menu"), desc="Open clipboard manager"),
-    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
-    Key([mod, "shift"], "space", lazy.window.toggle_floating(), desc="Toggle floating"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod, "shift"], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    #    Key([mod], "r", lazy.spawncmd(),
-    #        desc="Spawn a command using a prompt widget"),
-]
 
 # groups = [ Group(f"{i+1}", label="♥") for i in range(5)]
 groups = [Group(f"{i+1}", label="󰱺") for i in range(6)]
