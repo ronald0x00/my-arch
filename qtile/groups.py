6db37a4a0d3bfcf1af82e710d7fce013e932bb9b
Ronaldo <oldanorsh@gmail.com>
2024-01-03 14:04:42 +0000

    feat(groups): moving groups

---
+++ qtile/groups.py
@@ -0,0 +1,26 @@
+from libqtile.config import Group, Key
+from libqtile.lazy import lazy
+from keys import mod, keys
+
+groups = [Group(f"{i+1}", label="󰱺") for i in range(6)]
+
+
+for i in groups:
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+        ]
+    )
