3d0cd8e5f232294d1530d0f83331c5a0bd7de73b
Ronaldo <oldanorsh@gmail.com>
2024-01-04 08:05:09 +0000

    style(utils): moving color

---
+++ qtile/utils/color.py
@@ -0,0 +1,18 @@
+quirky = {
+    "bg": "#191724",
+    "fg": "#e0def4",
+    "fg_gutter": "#26233a",
+    "black": "#191724",
+    "red": "#eb6f92",
+    "green": "#31748f",
+    "yellow": "#f6c177",
+    "blue": "#5E4C86",
+    "magenta": "#67346b",
+    "cyan": "#555169",
+    "white": "#e0def4",
+    "orange": "#6e6a86",
+    "pink": "#7E4065",
+}
+
+# selected colorscheme
+colors = quirky
