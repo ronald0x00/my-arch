adc5e7a6632d8e6d4707961d624820a22841b1ae
Ronaldo <oldanorsh@gmail.com>
2024-01-10 09:07:22 +0000

    feat(utils): add new function for resize floating windows

--- qtile/utils/window.py
+++ qtile/utils/window.py
@@ -16,3 +16,19 @@ def move_up(qtile):
 
 def normalize_window(qtile):
     return qtile.current_window.set_size_floating(1000, 1000)
+
+
+def resize_left(qtile):
+    return qtile.current_window.resize_floating(-100, 0)
+
+
+def resize_right(qtile):
+    return qtile.current_window.resize_floating(100, 0)
+
+
+def resize_down(qtile):
+    return qtile.current_window.resize_floating(0, 100)
+
+
+def resize_up(qtile):
+    return qtile.current_window.resize_floating(0, -100)
