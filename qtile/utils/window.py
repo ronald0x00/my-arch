a1c5d3ccc7445e33410346c4ea0b5db4e8c5d0d2
Ronaldo <oldanorsh@gmail.com>
2024-01-12 07:26:46 +0000

    feat(utils/window): add move function for floating window

--- qtile/utils/window.py
+++ qtile/utils/window.py
@@ -9,6 +9,12 @@ default_directions = {
 
 
 @lazy.function
+def move(qtile, direction):
+    if qtile.current_window.floating:
+        qtile.current_window.cmd_move_floating(*default_directions[direction])
+
+
+@lazy.function
 def resize(qtile, direction):
     if qtile.current_window.floating:
         qtile.current_window.cmd_resize_floating(*default_directions[direction])
