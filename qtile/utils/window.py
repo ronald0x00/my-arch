dd36f4b9901627129585facd65c58ffc5b741dbc
Ronaldo <oldanorsh@gmail.com>
2024-01-12 07:15:54 +0000

    refactor(utils): refactor resize function

--- qtile/utils/window.py
+++ qtile/utils/window.py
@@ -1,34 +1,14 @@
-def move_left(qtile):
-    return qtile.current_window.move_floating(-100, 0)
+from libqtile.command import lazy
+
+default_directions = {
+    "left": (-10, 0),
+    "up": (0, -10),
+    "right": (10, 0),
+    "down": (0, 10),
+}
 
 
-def move_right(qtile):
-    return qtile.current_window.move_floating(100, 0)
-
-
-def move_down(qtile):
-    return qtile.current_window.move_floating(0, 100)
-
-
-def move_up(qtile):
-    return qtile.current_window.move_floating(0, -100)
-
-
-def normalize_window(qtile):
-    return qtile.current_window.set_size_floating(1000, 1000)
-
-
-def resize_left(qtile):
-    return qtile.current_window.resize_floating(-100, 0)
-
-
-def resize_right(qtile):
-    return qtile.current_window.resize_floating(100, 0)
-
-
-def resize_down(qtile):
-    return qtile.current_window.resize_floating(0, 100)
-
-
-def resize_up(qtile):
-    return qtile.current_window.resize_floating(0, -100)
+@lazy.function
+def resize(qtile, direction):
+    if qtile.current_window.floating:
+        qtile.current_window.cmd_resize_floating(*default_directions[direction])
