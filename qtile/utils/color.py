d092b704d9c5e54ae9c43ce3f3a369a5c3750806
Ronaldo <oldanorsh@gmail.com>
2024-01-16 13:02:43 -0300

    style(colors): update color

--- qtile/utils/color.py
+++ qtile/utils/color.py
@@ -2,15 +2,15 @@ quirky = {
     "bg": "#191724",
     "fg": "#e0def4",
     "fg_gutter": "#26233a",
-    "black": "#191724",
+    "black": "#1f1d2e",
     "red": "#82032d",
-    "green": "#31748f",
+    "green": "#797cad",
     "yellow": "#f6c177",
     "blue": "#5E4C86",
     "magenta": "#67346b",
     "cyan": "#555169",
     "white": "#e0def4",
-    "orange": "#6e6a86",
+    "orange": "#AA9894",
     "pink": "#7E4065",
 }
 
